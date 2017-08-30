67b639bf79e85c90f23548fcd8b1bfc562bf850c
Adam Stooke <adam.stooke@gmail.com>
2017-08-29 17:41:05 -0700

    large_data_npshmemarray_read test

--- tests/large_data_mp_read.py
+++ tests/large_data_mp_read.py
@@ -2,8 +2,8 @@
 import multiprocessing as mp
 import numpy as np
 import time
-# import synkhronos
-# from synkhronos.shmemarray import NpShmemArray
+import synkhronos
+from synkhronos.shmemarray import NpShmemArray
 
 N = 1000
 C = 3
@@ -16,7 +16,6 @@ G = 8
 
 def main():
 
-    # x = NpShmemArray("float32", (N, C, H, W), )
     x = np.ctypeslib.as_array(mp.RawArray('f', N * C * H * W)).reshape(N, C, H, W)
     print(x.shape)
 
@@ -35,15 +34,26 @@ def main():
 def worker(x, b, rank):
 
     rand_idxs = np.random.randint(low=0, high=N, size=(K, M))
+    total_t = 0.
+    if rank == 0:
+        y = NpShmemArray("float32", (N, C, H, W), "test_tag", create=True)
+        b.wait()
+    else:
+        b.wait()
+        y = NpShmemArray("float32", (N, C, H, W), "test_tag", create=False)
     b.wait()
     for i in range(K):
         idxs = rand_idxs[i]
         t_0 = time.time()
-        new_mat = x[idxs]
+        new_mat = y[idxs]
+        # new_mat = x[idxs]
         t_1 = time.time()
         new_mat += 1  # (do something with it to make sure it's real)
-        print(rank, "copy time {}: {:.7f}".format(i, t_1 - t_0))
+        elapse = t_1 - t_0
+        total_t += elapse
+        print(rank, "copy time {}: {:.7f}".format(i, elapse))
         b.wait()
+    print(rank, "total time: {:.4f}".format(total_t))
 
 
 if __name__ == "__main__":
